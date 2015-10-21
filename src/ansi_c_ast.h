262812d82b15ca7aedfa597e6ce04c97ffec5ca2
Byron Johnson <byron@byronjohnson.net>
2015-10-21 16:52:36 -0500

    ansi_c_ast: snapshot of partially implemented AST structures.

--- src/ansi_c_ast.h
+++ src/ansi_c_ast.h
@@ -35,9 +35,191 @@
  * ------
  */
 
-
 #ifndef ANSI_C_AST_H
 #define ANSI_C_AST_H
 #include "base.h"
+
+#ifdef TODO
+
+#define AC_INDEX_BITS 42
+typedef unsigned long ac_index_t;
+
+/* ---------------------------------------------------------------- */
+/* String fragments. */
+
+#define AC_STR_IOTA_BUF_SIZE 64
+struct ac_str_iota_s
+{
+  unsigned char buf[AC_STR_IOTA_BUF_SIZE];
+  size_t        size;  /* Total size of all bits, starting with element. */
+
+  ac_index_t    next_i : AC_INDEX_BITS;
+};
+typedef struct ac_str_iota_s ac_str_iota_t;
+
+#define AC_STR_SMALL_BUF_SIZE 1024
+struct ac_str_small_s
+{
+  unsigned char buf[AC_STR_SMALL_BUF_SIZE];
+  size_t        size;  /* Total size of all bits, starting with element. */
+
+  ac_index_t    next_i : AC_INDEX_BITS;
+};
+typedef struct ac_str_small_s ac_str_small_t;
+
+#define AC_STR_CHUNK_BUF_SIZE (32 * 1024)
+struct ac_str_chunk_s
+{
+  unsigned char buf[AC_STR_CHUNK_BUF_SIZE];
+  size_t        size;  /* Total size of all bits, starting with element. */
+
+  ac_index_t    next_i : AC_INDEX_BITS;
+};
+typedef struct ac_str_chunk_s ac_str_chunk_t;
+
+struct ac_str_s
+{
+  enum ac_str_type_e
+  { ac_str_type_iota  = (0 << 1) | (0 << 0)
+  , ac_str_type_small = (0 << 1) | (1 << 0)
+
+  , ac_str_type_chunk = (1 << 1) | (0 << 0)
+  } type       : 2;
+  ac_index_t i : AC_INDEX_T_BITS;
+  ac_index_t   : (64 - 2 - AC_INDEX_BITS);
+};
+typedef struct ac_str_s ac_str_t;
+
+/* ---------------------------------------------------------------- */
+/* Annotations. */
+
+/*
+ * Arbitrary string of no particular type.
+ *
+ * Can be used to represent an arbitrary string between some, but not all,
+ * parts of an AST.
+ */
+struct ac_untyped_str_s
+{
+  ac_index_t str_i : AC_INDEX_BITS;
+};
+typedef struct ac_untyped_str_s ac_untyped_str_t;
+
+/*
+ * Whitespace that can be arbitrarily formatted.
+ */
+struct ac_whitespace_implicit_s
+{
+};
+typedef struct ac_whitespace_implicit_s ac_whitespace_implicit_t;
+
+/*
+ * Explicitly formatted whitespace.
+ */
+struct ac_whitespace_explicit_s
+{
+  ac_index_t str_i : AC_INDEX_BITS;
+};
+typedef struct ac_whitespace_explicit_s ac_whitespace_explicit_t;
+
+/*
+ * Whitespace.
+ */
+struct ac_whitespace_s
+{
+  enum ac_whitespace_type_e
+  { ac_whitespace_type_implicit = (0 << 0)
+  , ac_whitespace_type_explicit = (1 << 0)
+  } type       : 1;
+  ac_index_t i : AC_INDEX_T_BITS;
+  ac_index_t   : (64 - 1 - AC_INDEX_BITS);
+};
+typedef struct ac_whitespace_s ac_whitespace_t;
+
+/*
+ * Comment.
+ */
+struct ac_whitespace_implicit_s
+{
+};
+typedef struct ac_whitespace_implicit_s ac_whitespace_implicit_t;
+
+/*
+ * Whitespace, comment, CPP, or fixed string.
+ */
+struct ac_annotation_s
+{
+  enum ac_annotation_type_e
+  { ac_annotation_type_untyped_str (0 << 1) | (0 << 0)
+  , ac_annotation_type_whitespace  (0 << 1) | (1 << 0)
+
+  , ac_annotation_type_comment     (1 << 1) | (0 << 0)
+  } type       : 2;
+  ac_index_t i : AC_INDEX_T_BITS;
+  ac_index_t   : (64 - 2 - AC_INDEX_BITS);
+};
+typedef struct ac_annotation_s ac_annotatoin_t;
+
+/* ---------------------------------------------------------------- */
+/* "Items": top-level declarations. */
+
+/*
+ * Item: a top-level declaration.
+ */
+struct ac_item_s
+{
+};
+typedef struct ac_item_s ac_item_t;
+
+/* ---------------------------------------------------------------- */
+/* TODO. */
+
+struct ac_ast_s
+{
+  ac_item_t *items;
+  size_t     items_n;
+};
+typedef struct ac_ast_s ac_ast_t;
+
+
+
+
+
+
+
+
+
+/*
+ * todo
+ */
+struct ac_ast_s
+{
+  ac_ast_chunk_t foo;
+};
+typedef struct ac_ast_s ac_ast_t;
+
+
+
+
+/* ---------------------------------------------------------------- */
+/* typedefs. */
+
+typedef struct ac_ast_s ac_ast_t;
+typedef struct ac_ast_chunk_s ac_ast_chunk_t;
+
+/* ---------------------------------------------------------------- */
+/* struct definitions. */
+
+/*
+ * annotation:
+ *
+ * Comment, CPP, or custom whitespace.
+ */
+
+struct ac_ast_chunk_s
+{
+};
+
+#endif /* TODO */
 
 #endif /* ifndef ANSI_C_AST_H */
