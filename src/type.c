3acae9d1739ca2f2b75834f5fdf0ecfad1bced87
Byron Johnson <byron@byronjohnson.net>
2015-10-24 12:58:54 -0500

    type: Basic structure.

--- src/type.c
+++ src/type.c
@@ -30,7 +30,550 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* stddef.h:
+ *   - NULL
+ */
+#include <stddef.h>
+
+/* stdlib.h:
+ *   - free
+ *   - malloc
+ */
+#include <stdlib.h>
+
 #include "base.h"
 #include "type.h"
 
-void type_placeholder(void) { }
+/* TODO */
+#ifdef TODO
+
+/* ---------------------------------------------------------------- */
+/* The type "type_t".                                               */
+/* ---------------------------------------------------------------- */
+
+static type_t             *(*type_id)              (type_t *self);
+static type_t             *(*type_init_type)       (type_t *self);
+static void                (*type_init)            (type_t *self, type_t *val,                  const type_t *initialization);
+static void                (*type_free)            (type_t *self, type_t *val);
+static int                 (*type_verify_init)     (type_t *self, const type_t *initialization, char *out_err_msg,          size_t err_msg_size);
+static type_t             *(*type_new)             (type_t *self, const type_t *initialization);
+static void                (*type_deep_dup)        (type_t *self, type_t *dest,                 const type_t *src);
+static void                (*type_shallow_dup)     (type_t *self, type_t *dest,                 const type_t *src);
+static void                (*type_direct_dup)      (type_t *self, type_t *dest,                 const type_t *src);
+static type_t             *(*type_new_deep_copy)   (type_t *self, const type_t *src);
+static type_t             *(*type_new_shallow_copy)(type_t *self, const type_t *src);
+static type_t             *(*type_new_direct_copy) (type_t *self, const type_t *src);
+static const char        **(*type_name)            (type_t *self);
+static const char        **(*type_info)            (type_t *self);
+static size_t              (*type_size)            (type_t *self);
+static int                 (*type_valid_tval)      (type_t *self);
+static void               *(*type_user_buf)        (type_t *self);
+static size_t              (*type_user_buf_num)    (type_t *self);
+static size_t              (*type_user_buf_size)   (type_t *self);
+
+static unsigned char         type_user_static[USER_STATIC_SIZE];
+static size_t                type_user_static_num   = sizeof(type_user_static) / sizeof(type_user_static[0]);
+static size_t                type_user_static_size  = sizeof(type_user_static);
+static void                 *type_user_dynamic      = NULL;
+static size_t                type_user_dynamic_size = 0;
+static type_heap_info_t      type_heap_info         = type_heap_none;
+
+const type_t type_type =
+  { &type_type
+
+  , type_id
+  , type_init_type
+  , type_init
+  , type_free
+  , type_verify_init
+  , type_new
+  , type_deep_dup
+  , type_shallow_dup
+  , type_direct_dup
+  , type_new_deep_copy
+  , type_new_shallow_copy
+  , type_new_direct_copy
+  , type_name
+  , type_info
+  , type_size
+  , type_size
+  , type_valid_tval
+  , type_user_buf
+  , type_user_buf_size
+
+  , type_user_static
+  , type_user_static_num;
+  , type_user_static_size;
+  , type_user_dynamic;    
+  , type_user_dynamic_size;
+  , type_heap_info;
+  };
+
+/* ---------------------------------------------------------------- */
+/* The type "type_init_t".                                          */
+/* ---------------------------------------------------------------- */
+
+static type_t             *(*type_init_id)              (type_t *self);
+static type_t             *(*type_init_init_type)       (type_t *self);
+static void                (*type_init_init)            (type_t *self, type_init_t *val,                  const type_init_t *initialization);
+static void                (*type_init_free)            (type_t *self, type_init_t *val);
+static int                 (*type_init_verify_init)     (type_t *self, const type_init_t *initialization, char *out_err_msg,          size_t err_msg_size);
+static type_init_t        *(*type_init_new)             (type_t *self, const type_init_t *initialization);
+static void                (*type_init_deep_dup)        (type_t *self, type_init_t *dest,                 const type_init_t *src);
+static void                (*type_init_shallow_dup)     (type_t *self, type_init_t *dest,                 const type_init_t *src);
+static void                (*type_init_direct_dup)      (type_t *self, type_init_t *dest,                 const type_init_t *src);
+static type_init_t        *(*type_init_new_deep_copy)   (type_t *self, const type_init_t *src);
+static type_init_t        *(*type_init_new_shallow_copy)(type_t *self, const type_init_t *src);
+static type_init_t        *(*type_init_new_direct_copy) (type_t *self, const type_init_t *src);
+static const char        **(*type_init_name)            (type_t *self);
+static const char        **(*type_init_info)            (type_t *self);
+static size_t              (*type_init_size)            (type_t *self);
+static int                 (*type_init_valid_tval)      (type_t *self);
+static void               *(*type_init_user_buf)        (type_t *self);
+static size_t              (*type_init_user_buf_size)   (type_t *self);
+
+static unsigned char         type_init_user_static[USER_STATIC_SIZE];
+static size_t                type_init_user_static_num   = sizeof(type_user_static) / sizeof(type_user_static[0]);
+static size_t                type_init_user_static_size  = sizeof(type_user_static);
+static void                 *type_init_user_dynamic      = NULL;
+static size_t                type_init_user_dynamic_size = 0;
+static type_heap_info_t      type_init_heap_info         = type_heap_none;
+
+const type_t type_init_type =
+  { &type_type
+
+  , type_init_id
+  , type_init_init_type
+  , type_init_init
+  , type_init_free
+  , type_init_verify_init
+  , type_init_new
+  , type_init_deep_dup
+  , type_init_shallow_dup
+  , type_init_direct_dup
+  , type_init_new_deep_copy
+  , type_init_new_shallow_copy
+  , type_init_new_direct_copy
+  , type_init_name
+  , type_init_info
+  , type_init_size
+  , type_init_size
+  , type_init_valid_tval
+  , type_init_user_buf
+  , type_init_user_buf_size
+
+  , type_init_user_static
+  , type_init_user_static_num;
+  , type_init_user_static_size;
+  , type_init_user_dynamic;    
+  , type_init_user_dynamic_size;
+  , type_init_heap_info;
+  };
+
+/* ---------------------------------------------------------------- */
+/* "type_t" definitions.                                            */
+/* ---------------------------------------------------------------- */
+
+static type_t             *(*type_id)              (type_t *self)
+{
+  return self;
+}
+
+static type_t             *(*type_init_type)       (type_t *self)
+{
+  return type_init_type;
+}
+
+static void                (*type_init)            (type_t *self, type_t *val,                  const type_t *initialization)
+{
+  TODO
+}
+
+static void                (*type_free)            (type_t *self, type_t *val)
+{
+  if (val->heap_info & type_heap_dynamic)
+  {
+    val->user_dynamic_size = 0;
+
+    val->heap_info &= ~ type_heap_dynamic;
+    free(val->user_dynamic);
+    val->user_dynamic = NULL;
+  }
+
+  if (val->heap_info & type_heap_self)
+  {
+    val->heap_info &= ~ type_heap_self;
+    free(val);
+    val = NULL;
+
+    return;
+  }
+}
+
+static int                 (*type_verify_init)     (type_t *self, const type_t *initialization, char *out_err_msg,          size_t err_msg_size)
+{
+  TODO
+}
+
+static type_t             *(*type_new)             (type_t *self, const type_t *initialization)
+{
+  TODO
+}
+
+static void                (*type_deep_dup)        (type_t *self, type_t *dest,                 const type_t *src)
+{
+  TODO
+}
+
+static void                (*type_shallow_dup)     (type_t *self, type_t *dest,                 const type_t *src)
+{
+  TODO
+}
+
+static void                (*type_direct_dup)      (type_t *self, type_t *dest,                 const type_t *src)
+{
+  TODO
+}
+
+static type_t             *(*type_new_deep_copy)   (type_t *self, const type_t *src)
+{
+  TODO
+}
+
+static type_t             *(*type_new_shallow_copy)(type_t *self, const type_t *src)
+{
+  TODO
+}
+
+static type_t             *(*type_new_direct_copy) (type_t *self, const type_t *src)
+{
+  TODO
+}
+
+static const char        **(*type_name)            (type_t *self)
+{
+  return "type";
+}
+
+static const char        **(*type_info)            (type_t *self)
+{
+  return "typedef struct type_s type_t";
+}
+
+static size_t              (*type_size)            (type_t *self)
+{
+  return ;
+}
+
+static int                 (*type_valid_tval)      (type_t *self)
+{
+  return TYPE_VALID_TVAL;
+}
+
+static void               *(*type_user_buf)        (type_t *self)
+{
+  return NULL;
+}
+
+static size_t              (*type_user_buf_num)    (type_t *self)
+{
+  return 0;
+}
+
+static size_t              (*type_user_buf_size)   (type_t *self)
+{
+  return 0;
+}
+
+/* ---------------------------------------------------------------- */
+/* "type_init_t" definitions.                                       */
+/* ---------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+type_t             *(*type_id)              (type_t *self);
+type_t             *(*type_init_type)       (type_t *self);
+void                (*type_init)            (type_t *self, tval *val,                  const tval *initialization);
+void                (*type_free)            (type_t *self, tval *val);
+int                 (*type_verify_init)     (type_t *self, const tval *initialization, char *out_err_msg,          size_t err_msg_size);
+tval               *(*type_new)             (type_t *self, const tval *initialization);
+void                (*type_deep_dup)        (type_t *self, tval *dest,                 const tval *src);
+void                (*type_shallow_dup)     (type_t *self, tval *dest,                 const tval *src);
+void                (*type_direct_dup)      (type_t *self, tval *dest,                 const tval *src);
+tval               *(*type_new_deep_copy)   (type_t *self, const tval *src);
+tval               *(*type_new_shallow_copy)(type_t *self, const tval *src);
+const char        **(*type_name)            (type_t *self);
+const char        **(*type_info)            (type_t *self);
+size_t              (*type_size)            (type_t *self);
+int                 (*type_valid_tval)      (type_t *self);
+void               *(*type_user_buf)        (type_t *self);
+size_t              (*type_user_buf_size)   (type_t *self);
+unsigned char         type_user_static[USER_STATIC_SIZE];
+size_t                type_user_static_size;
+void                 *type_user_dynamic;
+size_t                type_user_dynamic_size;
+
+
+static const unsigned char user_static_type[] = { 0 };
+static const size_t        user_static_type_size = sizeof(user_static_type) / sizeof(user_static_type[0]);
+
+static const unsigned char user_static_type_init[] = { 0 };
+static const size_t        user_static_type_init_size = sizeof(user_static_type_init) / sizeof(user_static_type_init[0]);
+
+/* The type "type_t". */
+
+/* The type "type_init_t". */
+const type_t type_init_type =
+  { &type_init_type
+
+  , &type_init_type
+
+  , init_type_init
+  , free_type_init
+
+  , new_type_init
+
+  , deep_dup_type_init
+  , shallow_dup_type_init
+  , direct_dup_type_init
+
+  , new_deep_copy_type_init
+  , new_shallow_copy_type_init
+  , new_direct_copy_type_init
+
+
+  , valid_tval_type_init
+
+
+  , type_name_type_init
+  , type_info_type_init
+  , type_size_type_init
+
+  , user_static_type_init
+  , user_static_size_type_init
+
+  , NULL
+  , 0
+  };
+
+/* ---------------------------------------------------------------- */
+
+void init_type(type_t *self, type_t *val, const type_init_t *initialization)
+{
+  val->type = self;
+}
+
+void free_type(type_t *self, type_t *val)
+{
+}
+
+
+type_t *new_type(type_t *self, const type_init_t *initialization)
+{
+  type_t *type;
+
+  type = malloc(self->type_size(self));
+
+  if (!type)
+  {
+    return NULL;
+  }
+
+  type_init(self, type, initialization);
+  ((unsigned char *) type->user_static)[0] = TYPE_ON_HEAP;
+
+  return type;
+}
+
+void dup_type(type_t *self, type_t *dest, const type_t *src)
+{
+  dest->type = src->type;
+
+  /* ---------------------------------------------------------------- */
+
+  dest->init_type         = src->init_type;
+
+  dest->init              = src->init;
+  dest->free              = src->free;
+
+  dest->new               = src->new;
+
+  dest->dup               = src->dup;
+
+
+  dest->type_name         = src->type_name;
+  dest->type_info         = src->type_info;
+  dest->type_size         = src->type_size;
+
+  dest->user_static       = src->user_static;
+  dest->user_static_size  = src->user_static_size;
+
+  dest->user_dynamic      = src->user_dynamic;
+  dest->user_dynamic_size = src->user_dynamic_size;
+}
+
+
+
+const char *type_name_type(type_t *self)
+{
+  return "type";
+}
+
+const char *type_info_type(type_t *self)
+{
+  return "typedef struct type_s type_t";
+}
+
+size_t type_size_type(type_t *self)
+{
+  return sizeof(type_t);
+}
+
+/* ---------------------------------------------------------------- */
+
+void init_type_init(type_t *self, type_t *val, const type_init_t *initialization)
+{
+  val->type = self;
+
+  if (!initialization)
+  {
+    dup_type_init(val, default_init_type);
+
+    return;
+  }
+  else
+  {
+    dup_type_init(val, initialization);
+
+    return;
+  }
+}
+
+void free_type_init(type_t *self, type_init_t *val)
+{
+}
+
+
+type_init_t *new_type_init(type_t *self, const type_init_t *initialization)
+{
+  type_init_t *type_init;
+
+  type_init = malloc(self->type_size(self));
+
+  if (!type_init)
+  {
+    return NULL;
+  }
+
+  type_init_init(self, type_init, initialization);
+  ((unsigned char *) type_init->user_static)[0] = TYPE_INIT_ON_HEAP;
+
+  return type_init;
+}
+
+void dup_type_init(type_t *self, type_init_t *dest, const type_init_t *src)
+{
+  dest->type = src->type;
+
+  /* ---------------------------------------------------------------- */
+
+  dest->override_init              = src->override_init;
+  dest->override_free              = src->override_free;
+  dest->override_new               = src->override_new;
+  dest->override_dup               = src->override_dup;
+  dest->override_type_name         = src->override_type_name;
+  dest->override_type_info         = src->override_type_info;
+  dest->override_type_size         = src->override_type_size;
+  dest->override_user_static       = src->override_user_static;
+  dest->override_user_static_size  = src->override_user_static_size;
+  dest->override_user_dynamic      = src->user_dynamic;
+  dest->override_user_dynamic_size = src->user_dynamic_size;
+
+
+  dest->init                       = src->init;
+  dest->free                       = src->free;
+
+  dest->new                        = src->new;
+
+  dest->dup                        = src->dup;
+
+
+  dest->type_name                  = src->type_name;
+  dest->type_info                  = src->type_info;
+  dest->type_size                  = src->type_size;
+
+  dest->user_static                = src->user_static;
+  dest->user_static_size           = src->user_static_size;
+
+  dest->user_dynamic               = src->user_dynamic;
+  dest->user_dynamic_size          = src->user_dynamic_size;
+}
+
+
+
+const char *type_name_type_init(type_t *self)
+{
+  return "type_init"
+}
+
+const char *type_info_type_init(type_t *self)
+{
+  return "typedef struct type_init_s type_init_t"
+}
+
+size_t type_size_type_init(type_t *self)
+{
+  return sizeof(type_init_t);
+}
+/* TODO */
+#endif /* TODO */
--- src/type.h
+++ src/type.h
@@ -40,4 +40,255 @@
 #define TYPE_H
 #include "base.h"
 
+/*
+ * tval:
+ *
+ * Label for typed values.
+ *
+ * In the code:
+ *
+ * > tval *foo
+ *
+ * The type "tval" is intended to denote that the type used for "foo" is, or is
+ * equivalent to, a struct whose first field has type "type_t *".
+ *
+ * This allows users to obtain the type of "foo", by dereferencing the first
+ * field of "foo":
+ *
+ * > type_t *type_of_foo;
+ * >
+ * > type_of_foo = *((type_t **) foo);
+ *
+ * "type_t *" can also indirectly be the first field: a valid alternative is a
+ * concrete struct, but not a pointer to a concrete struct, whose first field
+ * has type "type_t *".
+ */
+typedef void tval;
+
+/* A structure that describes a "type", a structure with associated procedures
+ * for initialization and such.
+ *
+ * A "type_t" describes a struct.  Such structs should have the first field set
+ * to "type_t *".
+ */
+#define USER_STATIC_SIZE 256
+enum type_heap_info_e
+{
+  type_heap_none    = 0,
+
+  type_heap_self    = (0 << 1) | (1 << 0),
+  type_heap_dynamic = (1 << 1) | (0 << 0),
+
+  type_heap_dynamic_self
+                    = (1 << 1) | (1 << 1)
+};
+typedef enum type_heap_info_e type_heap_info_t;
+
+#define TYPE_VALID_TVAL        0
+#define TYPE_NOVALID_TVAL_GEN_1 1
+typedef struct type_s type_t;
+struct type_s
+{
+  /* Make "type_t" itself a type by setting the first field as "type_t *".  */
+  /* All initialized "type_t" values should have "type" set to "type_type". */
+  type_t         *type;
+
+  /* ---------------------------------------------------------------- */
+
+  /*
+   * Wherever "tval" occurs in "type_t" values, "tval" must be equal to the
+   * associated type that "type_t" describes.
+   */
+
+  /* The type of "tval *" parameters.  Should return "self". */
+  type_t             *(*id)              (type_t *self);
+
+  /* The type of "initialization" parameters. */
+  type_t             *(*init_type)       (type_t *self);
+
+  /* Initialization a value.  Some types support NULL "initialization"
+   * arguments.
+   */
+  void                (*init)            (type_t *self, tval *val,                  const tval *initialization);
+  void                (*free)            (type_t *self, tval *val);
+
+  /*
+   * Verify the initialization argument, which can be NULL, is valid.
+   *
+   * Should return "0" when valid, and anything else when not.
+   *
+   * NULL output arguments must be supported.
+   */
+  int                 (*verify_init)     (type_t *self, const tval *initialization, char *out_err_msg,          size_t err_msg_size);
+
+  /*
+   * It is recommended that invalid initialization arguments are cleanly
+   * handled.  "NULL" might (or might not) be returned for invalid
+   * initialization arguments.
+   */
+  tval               *(*new)             (type_t *self, const tval *initialization);
+
+  /*
+   * 3 Variants for copying data:
+   *
+   * These descriptions informally describe an intuition that depends on the
+   * contexts in which they are used.
+   *
+   * In particular, what constitutes a level of indirection is unspecified
+   * here, and what constitutes a level of indirection according to one type is
+   * not necessarily what constitutes a level of indirection according to
+   * another (but, particularly when that alternate type is a
+   * "type_t"-associated type, it usually is).
+   *
+   * For example, "int **" could plausible be treated as each of the following:
+   *   1) A 4-4 matrix.
+   *
+   *   2) A reference to an "int *"-represented file handle.
+   *   3) A reference to a vector represented by 4 integers.
+   *
+   *   4) A reference to a reference of an integer.
+   *
+   * Importantly, each pointer "reference" could be considered *incidental* to
+   * a structure, e.g. as part of an efficient way to link two structures, but
+   * they could also be considered to be proper "references" that conceptually
+   * add a level of indirection.
+   *
+   * Depending on what the type considers a level of indirection for a field of
+   * this type, the 3 copying variants could do different things:
+   *   1): "direct":    dest =   src;
+   *       "shallow":  *dest =  *src;
+   *       "deep":    **dest = **src;
+   *
+   *   2),
+   *   3): "direct":    dest =   src;
+   *       "shallow":  *dest =  *src;
+   *       "deep":     *dest =  *src;
+   *
+   *   4): "direct":    dest =   src;
+   *       "shallow":   dest =   src;
+   *       "deep":      dest =   src;
+   *
+   * deep:
+   *   Mutating the state of either "src" or "src"'s dependents should not
+   *   affect the state of "dest" and its referents.
+   *
+   *   TODO
+   *
+   *   Referents, e.g. file handles or pointers considered as "references", can
+   *   still be copied as such.
+   *
+   * shallow:
+   *   Mutating the fields of "src" should not affect the fields of "dest".
+   *
+   *   The most flexibility is given to implementations of "shallow" cloning.
+   *
+   *   TODO
+   *
+   * direct:
+   *   All references and values should be TODO
+   * If "src" is at first independent from "dest"
+   */
+  void                (*deep_dup)        (type_t *self, tval *dest,                 const tval *src);
+  void                (*shallow_dup)     (type_t *self, tval *dest,                 const tval *src);
+  void                (*direct_dup)      (type_t *self, tval *dest,                 const tval *src);
+
+  tval               *(*new_deep_copy)   (type_t *self, const tval *src);
+  tval               *(*new_shallow_copy)(type_t *self, const tval *src);
+  tval               *(*new_direct_copy) (type_t *self, const tval *src);
+
+
+  const char        **(*name)            (type_t *self);
+  const char        **(*info)            (type_t *self);
+  size_t              (*size)            (type_t *self);
+
+  /*
+   * Whether values of this type are necessarily valid "tval"s.
+   *
+   * This should be "false" if the described type doesn't always contain
+   * "type_t *" in the first field.
+   */
+  int                 (*valid_tval)      (type_t *self);
+
+  void               *(*user_bufTODO)        (type_t *self);
+  size_t              (*user_bufTODO_size)   (type_t *self);
+
+
+  unsigned char         user_static[USER_STATIC_SIZE];
+  size_t                user_static_num;
+  size_t                user_static_size;
+
+  void                 *user_buf;
+  size_t                user_buf_size;
+
+  type_heap_info_t      heap_info;
+};
+
+/* TODO */
+#ifdef TODO
+extern const type_t type_type;
+
+typedef struct type_init_s type_init_t;
+struct type_init_t
+{
+  type_t *type;
+
+  /* ---------------------------------------------------------------- */
+  /* Minimum required arguments to construct a "type_t".              */
+  /* ---------------------------------------------------------------- */
+
+  /* ---------------------------------------------------------------- */
+  /* Optional arguments.                                              */
+  /* ---------------------------------------------------------------- */
+
+  int override_id;
+  int override_init_type;
+  int override_init;
+  int override_free;
+  int override_verify_init;
+  int override_new;
+  int override_deep_dup;
+  int override_shallow_dup;
+  int override_new_deep_copy;
+  int override_new_shallow_copy;
+  int override_new_direct_copy;
+  int override_name;
+  int override_info;
+  int override_size;
+  int override_valid_tval;
+  int override_user_static;
+  int override_user_static_size;
+  int override_user_buf;
+  int override_user_buf_size;
+
+  type_t             *(*id)              (type_t *self);
+  type_t             *(*init_type)       (type_t *self);
+  void                (*init)            (type_t *self, tval *val,                  const tval *initialization);
+  void                (*free)            (type_t *self, tval *val);
+  int                 (*verify_init)     (type_t *self, const tval *initialization, char *out_err_msg,          size_t err_msg_size);
+  tval               *(*new)             (type_t *self, const tval *initialization);
+  void                (*deep_dup)        (type_t *self, tval *dest,                 const tval *src);
+  void                (*shallow_dup)     (type_t *self, tval *dest,                 const tval *src);
+  void                (*direct_dup)      (type_t *self, tval *dest,                 const tval *src);
+  tval               *(*new_deep_copy)   (type_t *self, const tval *src);
+  tval               *(*new_shallow_copy)(type_t *self, const tval *src);
+  tval               *(*new_direct_copy) (type_t *self, const tval *src);
+  const char        **(*name)            (type_t *self);
+  const char        **(*info)            (type_t *self);
+  size_t              (*size)            (type_t *self);
+  int                 (*valid_tval)      (type_t *self);
+  void               *(*user_buf)        (type_t *self);
+  size_t              (*user_buf_size)   (type_t *self);
+  unsigned char         user_static[USER_STATIC_SIZE];
+  size_t                user_static_size;
+  void                 *user_buf;
+  size_t                user_buf_size;
+};
+
+extern const type_t type_init_type;
+
+/* TODO */
+#endif /* TODO */
+
+/* ---------------------------------------------------------------- */
+
 #endif /* ifndef TYPE_H */
