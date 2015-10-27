ea168fdb6b5d46e9c5e362435d7a5ca22ec69ff0
Byron Johnson <byron@byronjohnson.net>
2015-10-26 19:56:36 -0500

    Add new templated module: type_util.

--- src/tests/test_type_util.c
+++ src/tests/test_type_util.c
@@ -1,5 +1,5 @@
 /*
- * opencurry: tests/test_all.c
+ * opencurry: tests/test_type_util.c
  *
  * Copyright (c) 2015, Byron James Johnson
  * All rights reserved.
@@ -30,74 +30,35 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* stddef.h:
- *   - NULL
- */
-#include <stddef.h>
-
 #include "../base.h"
 #include "testing.h"
-#include "test_all.h"
-
-#include "test_testing.h"
-#include "test_resources.h"
-#include "test_opencurry.h"
-#include "test_cli.h"
-#include "test_util.h"
-#include "test_mempool.h"
-#include "test_type_base.h"
-#include "test_type_structs.h"
-#include "test_type_adts.h"
 #include "test_type_util.h"
-#include "test_bnf.h"
-#include "test_lex.h"
-#include "test_unicode.h"
-#include "test_utf8.h"
-#include "test_integer.h"
-#include "test_ansi_c_ast.h"
-#include "test_ansi_c_ast_base.h"
-#include "test_ansi_c_ast_gen.h"
-#include "test_ansi_c_ast_render.h"
 
-int test_all(int argc, char **argv)
+#include "../type_util.h"
+
+int test_type_util_cli(int argc, char **argv)
 {
-  return run_test_suite(test_all_test);
+  return run_test_suite(type_util_test);
 }
 
 /* ---------------------------------------------------------------- */
 
-/* Root unit test. */
-unit_test_t test_all_test =
-  {  test_all_run
-  , "test_all"
-  , "All tests."
+/* type_util tests. */
+unit_test_t type_util_test =
+  {  test_type_util_run
+  , "test_type_util"
+  , "type_util tests."
   };
 
-/* Array of all test groups. */
-unit_test_t *all_tests[] =
-  { &testing_test
-  , &resources_test
-  , &opencurry_test
-  , &cli_test
-  , &util_test
-  , &mempool_test
-  , &type_base_test
-  , &type_structs_test
-  , &type_adts_test
-  , &type_util_test
-  , &bnf_test
-  , &lex_test
-  , &unicode_test
-  , &utf8_test
-  , &integer_test
-  , &ansi_c_ast_test
-  , &ansi_c_ast_base_test
-  , &ansi_c_ast_gen_test
-  , &ansi_c_ast_render_test
-  , NULL
+/* Array of type_util tests. */
+unit_test_t *type_util_tests[] =
+  { NULL
   };
 
-unit_test_result_t test_all_run(unit_test_context_t *context)
+unit_test_result_t test_type_util_run(unit_test_context_t *context)
 {
-  return run_tests(context, all_tests);
+  return run_tests(context, type_util_tests);
 }
+
+/* ---------------------------------------------------------------- */
+
