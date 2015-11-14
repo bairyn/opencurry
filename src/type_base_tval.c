57032eb1abb13abaea5ee11a6edf706e388aebd9
Byron Johnson <byron@byronjohnson.net>
2015-11-13 19:30:54 -0600

    type_base_tval: Write documentation for "tval", and fix "typeof" procedures.

--- src/type_base_tval.c
+++ src/type_base_tval.c
@@ -37,38 +37,124 @@
 #include "type_base_type.h"
 #include "type_base_typed.h"
 
-#include "ptrs.h"
-
 /* ---------------------------------------------------------------- */
 /* tval                                                             */
 /* ---------------------------------------------------------------- */
 
 /*
- * Obtain the type of a "tval *".
+ * Obtain the type of a value.
  *
- * Only typed "tval"s are supported!
+ * "val" must be valid "tval" or NULL, but its polymorphically associated
+ * "typed_t" type can be NULL.
+ *
+ * If "val" or its type is NULL, this returns NULL.
+ */
+#ifdef TODO /* TODO: needs "type_container" implemented first. */
+typed_t typedof(const tval *val)
+{
+  const type_t *type_def;
+
+  if (!val)
+    return NULL;
+
+  type_def = typeof(val);
+  if (!type_def)
+    return NULL;
+
+  return type_container(type_def);
+}
+#endif /* #ifdef TODO */
+
+/*
+ * Obtain the type of a value.
+ *
+ * "val" must be valid "tval" or NULL, but its polymorphically associated
+ * "typed_t" type can be NULL.
+ *
+ * If "val" or its type is NULL, this returns NULL.
  */
 const type_t *typeof(const tval *val)
 {
-  return type_is_typed(typeof_indirect(val));
+  const type_t *indirect_type_def;
+
+  if (!val)
+    return NULL;
+
+  indirect_type_def = typeof_indirect(val);
+  if (!indirect_type_def)
+    return NULL;
+
+  return type_is_typed(indirect_type_def);
 }
 
 /*
  * Obtain the indirect type of a "tval *", without calling "type_is_typed" to
  * obtain a possibly more specific reference to the type.
+ *
+ * "val" can be NULL, and it can also have a NULL "typed_t" value, but it still
+ * must be a valid "tval".
+ *
+ * Ultimately, "(tval *) val" is interpreted as "(typed_t *) val", i.e. a
+ * pointer to a function that returns a "type_t".
  */
 const type_t *typeof_indirect(const tval *val)
 {
-  return (const type_t *) (tval_get_typed(val)());
+  typed_t typed;
+
+  if (!val)
+    return NULL;
+
+  typed = tval_get_typed(val);
+  if (!typed)
+    return NULL;
+
+  return (const type_t *) (typed());
+}
+
+/*
+ * Dereference the tval reference as a reference to "typed_t".
+ *
+ * Returns NULL if "val" is NULL.
+ *
+ * Also returns NULL if "val"'s "typed" type is NULL (i.e. when the value's
+ * relevant (first) "typed_t" field is NULL).
+ *
+ * Passing a reference to a value that is not a "tval" results in undefined
+ * behaviour.
+ *
+ * Generally, the type of the value is a struct whose first field has type
+ * "typed_t" (i.e., the value is in fact a "tval").
+ */
+typed_t tval_get_typed(const tval *val)
+{
+  const typed_t *typed_ref;
+  typed_t        typed;
+
+  if (!val)
+    return NULL;
+
+  typed_ref = (const typed_t *) (tval_to_typed(val));
+  if (!typed_ref)
+    return NULL;
+
+  typed = *typed_ref;
+  if (!typed)
+    return NULL;
+
+  return (typed_t) typed;
 }
 
 /*
  * "tval *"s are values that can be cast to "typed_t", generally because they
  * are structs whose first field has type "typed_t".
  *
- * Interpret the "tval *" as its type, "typed_t".
+ * Interpret a reference to a tval as a reference to a typed_t, i.e. the "tval
+ * *" as its type, "const typed_t *".
  */
-typed_t tval_get_typed(const tval *val)
+const typed_t *tval_to_typed(const tval *val)
 {
-  return (typed_t) (objpc_to_funpc((void *) val));
+  if (!val)
+    return NULL;
+
+  return (const typed_t *) val;
 }
--- src/type_base_tval.h
+++ src/type_base_tval.h
@@ -33,6 +33,8 @@
 /*
  * type_base_tval.h
  * ------
+ *
+ * tval: Values associated with a type representation.
  */
 
 #ifndef TYPE_BASE_TVAL_H
@@ -43,116 +45,227 @@
 #include "type_base_typed.h"
 
 /* ---------------------------------------------------------------- */
-/* tval                                                             */
+/* tval: Synopsis.                                                  */
 /* ---------------------------------------------------------------- */
 
-/* tval: opaque type TODO TODO. */
+/* tval: typed_t polymorphic values.                                */
 
-/*
- * tval:
- *
- * Label for so-called "typed values", values associated with a "type_t".
- *
- * "tvals" are generally values of a struct whose first field is "type_t *".
- *
- * Example:
- *
- * > extern type_t intpair_type;
- * >
- * > typedef struct intpair_s intpair_t;
- * > struct intpair_s
- * > {
- * >   type_t *type;
- * >
- * >   int     a;
- * >   int     b;
- * > };
- * >
- * > void debug_print(const char *fmt, ...);
- * >
- * > void debug_tval(const tval *val);
- * >
- * > int intpair_sum(intpair_t *pair, int is_debug)
- * > {
- * >   int sum;
- * >
- * >   sum = pair->a + pair->b;
- * >
- * >   if (is_debug)
- * >   {
- * >     debug_print
- * >       ( "intpair_sum:\n"
- * >         "  %d + %d => %d\n"
- * >       , pair->a
- * >       , pair->b
- * >       , sum
- * >       );
- * >
- * >     debug_tval(pair);
- * >   }
- * >
- * >   return sum;
- * > }
- * >
- * > void debug_tval(const tval *val)
- * > {
- * >   const type_t *type = (const type_t *) val;
- * >
- * >   size_t      type_size;
- * >   const char *type_name;
- * >
- * >   type_size = type->size(type);
- * >   type_name = type->name(type);
- * >
- * >   debug_print
- * >     ( "debug_tval:\n"
- * >       "  type name: %s\n"
- * >       "  type size: %d\n"
- * >     , (const char *) type_name
- * >     , (size_t)       type_size
- * >     );
- * > }
- * >
- * > static intpair_t my_intpair =
- * >   { intpair_type
- * >
- * >   , 7
- * >   , 8
- * >   };
- * >
- * > static tval_t *my_intpair_val  = &my_intpair;
- * > static type_t *my_intpair_type = (type_t *) my_intpair_val;
- *
- * ----------------------------------------------------------------
- *
- * A "tval" is a value of a struct whose first field is "type_t *", a reference
- * to the type .
- *
- * --
- *
- * Given:
- *
- * > tval *foo
- *
- * The type "tval" is intended to denote that the type used for "foo" is, or is
- * equivalent to, a struct whose first field has type "type_t *".
- *
- * This allows users to obtain the type of "foo", by dereferencing the first
- * field of "foo":
- *
- * > type_t *type_of_foo;
- * >
- * > type_of_foo = *((type_t **) foo);
- *
- * "type_t *" can also indirectly be the first field: a valid alternative is a
- * concrete struct, but not a pointer to a concrete struct, whose first field
- * has type "type_t *".
- */
+/* "tval *" is "void *" with the additional semantics that non-NULL */
+/* references are polymorphic with references to "typed_t"          */
+/*                                                                  */
+/* In other words, the "tval" label means that "tval *" pointers    */
+/* can be cast to "typed_t *" pointers.                             */
+/*                                                                  */
+/* Usually, a "tval *" refers to a value of a struct whose          */
+/* first field is "typed_t type", where the underlying memory       */
+/* of the struct value starts with th underlying memory of          */
+/* the value of tihs field.                                         */
+
+/* > typedef struct type_s struct_t;                                */
+/* > typedef const type_t *(*typed_t)(void);                        */
+/* >                                                                */
+/* > /-* Values of this type are valid "tval"s. *-/                 */
+/* > typedef struct mystruct_s mystruct_t;                          */
+/* > struct mystruct_s                                              */
+/* > {                                                              */
+/* >   typed_t type;                                                */
+/* >                                                                */
+/* >   int a;                                                       */
+/* >   int b;                                                       */
+/* >   ...                                                          */
+/* > };                                                             */
+
+/* ---------------------------------------------------------------- */
+/* tval: Documentation.                                             */
+/* ---------------------------------------------------------------- */
+
+/* Background: first-field polymorphism.                            */
+/*                                                                  */
+/* Given a struct, for example:                                     */
+/* > typedef struct intpair_s intpair_t;                            */
+/* > struct intpair_s                                               */
+/* > {                                                              */
+/* >   int a;                                                       */
+/* >   int b;                                                       */
+/* > };                                                             */
+/*                                                                  */
+/* Then a pointer to a value of the struct can also be              */
+/* interpreted as a pointer to the first field value of a struct,   */
+/* because the memory of a value of "intpair_t" begins with the     */
+/* first field.                                                     */
+/*                                                                  */
+/* For example:                                                     */
+/* > static const intpair_t pair_3_7 =                              */
+/* >   { /-* a *-/ 3                                                */
+/* >   , /-* b *-/ 7                                                */
+/* >   };                                                           */
+/* >                                                                */
+/* > static const intpair_t *my_pair = &pair_3_7;                   */
+/* >                                                                */
+/* > static const int * const my_a = (const int *) my_pair;         */
+/*                                                                  */
+/* Here "*my_a" would evaluate to 3.                                */
+/*                                                                  */
+/* A "tval *" uses this concept for type representation values      */
+/* as the first field rather than "int"s.                           */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* Background: type_t.                                              */
+/*                                                                  */
+/* "type_t" is a struct, "type_s".  A "type_t" value describes      */
+/* a type, containing data about how to handle initialization,      */
+/* memory allocation, etc.                                          */
+/*                                                                  */
+/* For a simpler API, we might define our "intpair_t" struct        */
+/* above with an additional "type_t *" field, to allow "intpair_t"  */
+/* values to polymorphically be cast to a "type_t *" pointer,       */
+/* using first-field polymorphism:                                  */
+/*                                                                  */
+/* > typedef struct intpair_s intpair_t;                            */
+/* > struct intpair_s                                               */
+/* > {                                                              */
+/* >   type_t *type;                                                */
+/* >                                                                */
+/* >   int a;                                                       */
+/* >   int b;                                                       */
+/* > };                                                             */
+/*                                                                  */
+/* We can also define a second struct conforming to the same        */
+/* first-field polyhorphism:                                        */
+/*                                                                  */
+/* > typedef struct errcode_s errcode_t;                            */
+/* > struct errcode_s                                               */
+/* > {                                                              */
+/* >   type_t *type;                                                */
+/* >                                                                */
+/* >   int  error_code;                                             */
+/* >   char error_msg[ERR_MSG_SIZE];                                */
+/* > };                                                             */
+/*                                                                  */
+/* This allows us to retrieve the first, "type_t *" field from      */
+/* a pointer to either "intpair_t" or "errcode_t':                  */
+/*                                                                  */
+/* > type_t **get_type(void *val)                                   */
+/* > {                                                              */
+/* >   void    *value;                                              */
+/* >   type_t **type;                                               */
+/* >                                                                */
+/* >   value = ((void    ) *) val;                                  */
+/* >   type  = ((type_t *) *) val;                                  */
+/* >                                                                */
+/* >   return type;                                                 */
+/* > }                                                              */
+/*                                                                  */
+/* Here we just reinterpret a pointer to either "intpair_t",        */
+/* "errcode_t", or any other conformant structs, as a pointer       */
+/* to "type_t *".                                                   */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* Background: typed_t.                                             */
+/*                                                                  */
+/* "type_t" is defined in the "type_base_type" module as a struct.  */
+/*                                                                  */
+/* However, the standard type of type representations for           */
+/* a given type is "typed_t", not "type_t"; this is defined         */
+/* in "type_base_typed".                                            */
+/*                                                                  */
+/* What is typed_t?                                                 */
+/*                                                                  */
+/* Many constant top-level type representations can be defined with */
+/* a static initializer, providing a "const type_t *" available for */
+/* use.                                                             */
+/*                                                                  */
+/* But static initializers have their limitations.  They can't,     */
+/* for example, call out to functions, nor can they refer to        */
+/* other variables.                                                 */
+/*                                                                  */
+/* Adding a layer of indirection via a call to a C function         */
+/* grants flexibility, that, for instance, supports both.           */
+/*                                                                  */
+/* This is the purpose of "typed_t".  A "typed_t" is a function     */
+/* pointer to a function that takes zero arguments and returns      */
+/* a "const type_t *" pointer*                                      */
+/*                                                                  */
+/* > typedef const type_t *(*typed_t)(void);                        */
+/* >                                                                */
+/* > const type_t *intpair_type(void);                              */
+/* >                                                                */
+/* > void foo()                                                     */
+/* > {                                                              */
+/* >   typed_t which_type = intpair_type;                           */
+/* >                                                                */
+/* >   const type_t *type_def = which_type();                       */
+/* >                                                                */
+/* >   do_something_with_type_def(type_def);                        */
+/* > }                                                              */
+/*                                                                  */
+/* Thus the standard reference to type descriptions (type_t)'s      */
+/* is not a pointer to "type_t", but a pointer to a function        */
+/* with this type:                                                  */
+/* > const type_t *foo_type(void);                                  */
+/*                                                                  */
+/* ("void" in the argument list means that the C function           */
+/* takes zero arguments.)                                           */
+/*                                                                  */
+/* We can obtain a "const type_t *" reference by just calling       */
+/* "foo_type", or a function pointer to "foo_type":                 */
+/* > void foo()                                                     */
+/* > {                                                              */
+/* >   const type_t *foo_type_def = foo_type();                     */
+/* >                                                                */
+/* >   do_something_with_type_def(foo_type_def);                    */
+/* > }                                                              */
+/*                                                                  */
+/* For users writing a type representation for a type, i.e.         */
+/* defining a "typed_t" pointer or the type of its reference:       */
+/* "const type_t foo_type(void);", they can still use static        */
+/* initializers to define a "type_t" when the requirements are      */
+/* simple enough, and just define "foo_type" to be a                */
+/* simple wrapper that directly returns a reference to this         */
+/* struct.                                                          */
+/*                                                                  */
+/* More complex type representation definitions will also work;     */
+/* users don't need to know the difference, since both kinds        */
+/* of definition provide the same "typed_t"-based type.             */
+/*                                                                  */
+/* ---------------------------------------------------------------- */
+/*                                                                  */
+/* What is a tval?                                                  */
+/*                                                                  */
+/* First-field polymorphism is the underlying idea behind "tvals".  */
+/*                                                                  */
+/* Internally, "tval *" is just typedef'd to "void *".              */
+/*                                                                  */
+/* Wherever a "tval *" occurs instead of just "void *",             */
+/* the pointer to the value is expected to be polymorphic with a    */
+/* pointer to "typed_t", interpreting "tval" as "typed_t".          */
+/*                                                                  */
+/* Thus, generally speaking, a "tval" value is a value of any       */
+/* struct type whose first field has type "typed_t".                */
+/* (More accurately, any value whose underlying memory starts with  */
+/* a valid representation of "typed_t", e.g. a "typed_t" function   */
+/* pointer itself.)                                                 */
+/*                                                                  */
+/* This isn't enforced internally, where "tval" is equivalent to    */
+/* "void" (and thus "tval *" to "void *"): "tval", then,            */
+/* is effectively a label that means that it is safe to interpret   */
+/* its memory as type "typed_t".                                    */
+
+/* ---------------------------------------------------------------- */
+/* tval: Values associated with a type, via "typed_t" polymorphism. */
+/* ---------------------------------------------------------------- */
+
 typedef void tval;
 
-const type_t *typeof         (const tval *val);
-const type_t *typeof_indirect(const tval *val);
-typed_t       tval_get_typed (const tval *val);
+#ifdef TODO /* TODO: needs "type_container" implemented first. */
+typed_t        typedof        (const tval *val);
+#endif /* #ifdef TODO */
+const type_t  *typeof         (const tval *val);
+const type_t  *typeof_indirect(const tval *val);
+typed_t        tval_get_typed (const tval *val);
+const typed_t *tval_to_typed  (const tval *val);
 
 /* ---------------------------------------------------------------- */
 /* Post-dependencies.                                               */
