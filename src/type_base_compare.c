48c0c5f7b24c608354a903d128bc2eebe82d96b1
Byron Johnson <byron@byronjohnson.net>
2015-11-15 21:40:08 -0600

    Implement comparers in type_base_compare.?.

--- src/type_base_compare.c
+++ src/type_base_compare.c
@@ -1,5 +1,5 @@
 /*
- * opencurry: compare.c
+ * opencurry: type_base_compare.c
  *
  * Copyright (c) 2015, Byron James Johnson
  * All rights reserved.
@@ -31,6 +31,626 @@
  */
 
 #include "base.h"
-#include "compare.h"
+#include "type_base_compare.h"
 
-void compare_placeholder(void) { }
+/* string.h:
+ *   - memcmp
+ */
+#include <string.h>
+
+/* stddef.h:
+ *   - NULL
+ *   - ptrdiff_t
+ *   - size_t
+ */
+#include <stddef.h>
+
+/* string.h:
+ *   - strcmp
+ *   - strncmp
+ *   - memmove
+ *   - memset
+ */
+#include <string.h>
+
+#include "base.h"
+#include "type_base_prim.h"
+#include "type_base_compare.h"
+
+#include "type_base_typed.h"
+#include "type_base_tval.h"
+#include "type_base_memory_tracker.h"
+#ifdef TODO
+#error "TODO: #include primitive c data types"
+#else  /* #ifdef TODO */
+#include "type_base.h"
+#endif /* #ifdef TODO */
+#include "type_base_type.h"
+
+#include "ptrs.h"
+
+/* ---------------------------------------------------------------- */
+/* Orderings: comparison descriptions.                              */
+/* ---------------------------------------------------------------- */
+
+int ordering_err_1   (void)
+{
+  return ORDERING_ERR_1;
+}
+
+int ordering_err_2   (void)
+{
+  return ORDERING_ERR_2;
+}
+
+int ordering_lt_lossy(void)
+{
+  return ORDERING_LT_LOSSY;
+}
+
+
+int is_ordering_success(int ordering)
+{
+  return IS_ORDERING_SUCCESS(ordering);
+}
+
+int is_ordering_error  (int ordering)
+{
+  return IS_ORDERING_ERROR  (ordering);
+}
+
+int is_ordering_lossy  (int ordering)
+{
+  return IS_ORDERING_LOSSY  (ordering);
+}
+
+
+/* If the result corresponds to an error code, return ORDERING_LT_LOSSY. */
+int ordering_success(int ordering)
+{
+  return ORDERING_SUCCESS(ordering);
+}
+
+int ordering_lt(void)
+{
+  return ORDERING_LT;
+}
+
+int ordering_eq(void)
+{
+  return ORDERING_EQ;
+}
+
+int ordering_gt(void)
+{
+  return ORDERING_GT;
+}
+
+
+ordering_relation_t ordering_relation(int ordering)
+{
+  return ORDERING_RELATION(ordering);
+}
+
+/* ---------------------------------------------------------------- */
+/* Comparers.                                                       */
+/* ---------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------- */
+/* Various comparers.                                               */
+/* ---------------------------------------------------------------- */
+
+/* with_type:     Elements are in the array.       */
+/* ref_with_type: Elements are pointers to values. */
+int compare_with_type    (void *context, const     void         *check, const          void         *baseline)
+{
+  const type_t *type = context;
+
+#if ERROR_CHECKING
+  if (!context)
+    return ordering_err_2();
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return cmp_with_type(type, check, baseline);
+}
+
+int compare_ref_with_type(void *context, const     void        **check, const          void        **baseline)
+{
+  const type_t *type = context;
+
+#if ERROR_CHECKING
+  if (!context)
+    return ordering_err_2();
+  if (!check || !baseline)
+    return ordering_err_2();
+  if (!*check || !*baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return cmp_with_type(type, *check, *baseline);
+}
+
+
+int compare_tval    (void *context, const          tval         *check, const          tval         *baseline)
+{
+  const type_t *type;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  type = typeof(check);
+#if ERROR_CHECKING
+  if (!type)
+    return ordering_err_2();
+  if (!is_type_equivalent(type, typeof(baseline)))
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return cmp_with_type(type, check, baseline);
+}
+
+int compare_tvalr   (void *context, const          tval        **check, const          tval        **baseline)
+{
+  const type_t *type;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+  if (!*check || !*baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  type = typeof(*check);
+#if ERROR_CHECKING
+  if (!type)
+    return ordering_err_2();
+  if (!is_type_equivalent(type, typeof(*baseline)))
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return cmp_with_type(type, *check, *baseline);
+}
+
+
+int compare_char    (void *context, const          char         *check, const          char         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_schar   (void *context, const signed   char         *check, const signed   char         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_uchar   (void *context, const unsigned char         *check, const unsigned char         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_short   (void *context, const          short        *check, const          short        *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_ushort  (void *context, const unsigned short        *check, const unsigned short        *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_int     (void *context, const          int          *check, const          int          *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_uint    (void *context, const unsigned int          *check, const unsigned int          *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_long    (void *context, const          long         *check, const          long         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_ulong   (void *context, const unsigned long         *check, const unsigned long         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_float   (void *context, const          float        *check, const          float        *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_double  (void *context, const          double       *check, const          double       *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_ldouble (void *context, const long     double       *check, const long     double       *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+
+int compare_size    (void *context, const          size_t       *check, const          size_t       *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_ptrdiff (void *context, const          ptrdiff_t    *check, const          ptrdiff_t    *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+
+int compare_objp    (void *context, const          void        **check, const          void        **baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+int compare_funp    (void *context, const          funp_cast_t  *check, const          funp_cast_t  *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_CMP(*check, *baseline);
+}
+
+
+/* mem:  Directly compare the elements' bytes in memory.                       */
+/* memr: Elements are "void *" pointers; compare the referent areas of memory. */
+int compare_mem     (void *context, const          void         *check, const          void         *baseline)
+{
+  size_t n;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  n = objp_to_size(context);
+
+  if (n <= 0)
+    return ORDERING_SUCCESS(ORDERING_EQ);
+
+  return ORDERING_SUCCESS(memcmp(check, baseline, n));
+}
+
+int compare_memr    (void *context, const          void        **check, const          void        **baseline)
+{
+  size_t n;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+  if (!*check || !*baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  n = objp_to_size(context);
+
+  if (n <= 0)
+    return ORDERING_SUCCESS(ORDERING_EQ);
+
+  return ORDERING_SUCCESS(memcmp(*check, *baseline, n));
+}
+
+
+/* The strings are contained in the elements themselves. */
+int compare_strn    (void *context, const          char         *check, const          char         *baseline)
+{
+  size_t n;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  n = objp_to_size(context);
+
+  if (n <= 0)
+    return ORDERING_SUCCESS(ORDERING_EQ);
+
+  return ORDERING_SUCCESS(strncmp(check, baseline, n));
+}
+
+int compare_strz    (void *context, const          char         *check, const          char         *baseline)
+{
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_SUCCESS(strcmp(check, baseline));
+}
+
+
+/* Elements are "const char *". */
+int compare_strnr   (void *context, const          char        **check, const          char        **baseline)
+{
+  size_t n;
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+  if (!*check || !*baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  n = objp_to_size(context);
+
+  if (n <= 0)
+    return ORDERING_SUCCESS(ORDERING_EQ);
+
+  return ORDERING_SUCCESS(strncmp(*check, *baseline, n));
+}
+
+int compare_strzr   (void *context, const          char        **check, const          char        **baseline)
+{
+
+#if ERROR_CHECKING
+  if (!check || !baseline)
+    return ordering_err_2();
+  if (!*check || !*baseline)
+    return ordering_err_2();
+#endif /* #if ERROR_CHECKING  */
+
+  return ORDERING_SUCCESS(strcmp(*check, *baseline));
+}
+
+/* ---------------------------------------------------------------- */
+
+void *compare_with_type_context    (const type_t *type)
+{
+  return (void *) type;
+}
+
+void *compare_ref_with_type_context(const type_t *type)
+{
+  return (void *) type;
+}
+
+
+void *compare_tval_context   (void)
+{
+  return NULL;
+}
+
+void *compare_tvalr_context  (void)
+{
+  return NULL;
+}
+
+
+void *compare_char_context   (void)
+{
+  return NULL;
+}
+
+void *compare_schar_context  (void)
+{
+  return NULL;
+}
+
+void *compare_uchar_context  (void)
+{
+  return NULL;
+}
+
+void *compare_short_context  (void)
+{
+  return NULL;
+}
+
+void *compare_ushort_context (void)
+{
+  return NULL;
+}
+
+void *compare_int_context    (void)
+{
+  return NULL;
+}
+
+void *compare_uint_context   (void)
+{
+  return NULL;
+}
+
+void *compare_long_context   (void)
+{
+  return NULL;
+}
+
+void *compare_ulong_context  (void)
+{
+  return NULL;
+}
+
+void *compare_float_context  (void)
+{
+  return NULL;
+}
+
+void *compare_double_context (void)
+{
+  return NULL;
+}
+
+void *compare_ldouble_context(void)
+{
+  return NULL;
+}
+
+
+void *compare_size_context   (void)
+{
+  return NULL;
+}
+
+void *compare_ptrdiff_context(void)
+{
+  return NULL;
+}
+
+
+void *compare_objp_context   (void)
+{
+  return NULL;
+}
+
+void *compare_funp_context   (void)
+{
+  return NULL;
+}
+
+
+void *compare_mem_context    (size_t n)
+{
+  return (void *) size_to_objp(n);
+}
+
+void *compare_memr_context   (size_t n)
+{
+  return (void *) size_to_objp(n);
+}
+
+
+void *compare_strn_context   (size_t n)
+{
+  return (void *) size_to_objp(n);
+}
+
+void *compare_strz_context   (void)
+{
+  return NULL;
+}
+
+
+void *compare_strnr_context  (size_t n)
+{
+  return (void *) size_to_objp(n);
+}
+
+void *compare_strzr_context  (void)
+{
+  return NULL;
+}
+
+/* ---------------------------------------------------------------- */
+
+const comparer_t comparer_with_type     = (comparer_t) &compare_with_type;
+const comparer_t comparer_ref_with_type = (comparer_t) &compare_ref_with_type;
+
+const comparer_t comparer_tval          = (comparer_t) &compare_tval;
+const comparer_t comparer_tvalr         = (comparer_t) &compare_tvalr;
+
+const comparer_t comparer_char          = (comparer_t) &compare_char;
+const comparer_t comparer_schar         = (comparer_t) &compare_schar;
+const comparer_t comparer_uchar         = (comparer_t) &compare_uchar;
+const comparer_t comparer_short         = (comparer_t) &compare_short;
+const comparer_t comparer_ushort        = (comparer_t) &compare_ushort;
+const comparer_t comparer_int           = (comparer_t) &compare_int;
+const comparer_t comparer_uint          = (comparer_t) &compare_uint;
+const comparer_t comparer_long          = (comparer_t) &compare_long;
+const comparer_t comparer_ulong         = (comparer_t) &compare_ulong;
+const comparer_t comparer_float         = (comparer_t) &compare_float;
+const comparer_t comparer_double        = (comparer_t) &compare_double;
+const comparer_t comparer_ldouble       = (comparer_t) &compare_ldouble;
+
+const comparer_t comparer_size          = (comparer_t) &compare_size;
+const comparer_t comparer_ptrdiff       = (comparer_t) &compare_ptrdiff;
+
+const comparer_t comparer_objp          = (comparer_t) &compare_objp;
+const comparer_t comparer_funp          = (comparer_t) &compare_funp;
+
+const comparer_t comparer_mem           = (comparer_t) &compare_mem;
+const comparer_t comparer_memr          = (comparer_t) &compare_memr;
+
+const comparer_t comparer_strn          = (comparer_t) &compare_strn;
+const comparer_t comparer_strz          = (comparer_t) &compare_strz;
+
+const comparer_t comparer_strnr         = (comparer_t) &compare_strnr;
+const comparer_t comparer_strzr         = (comparer_t) &compare_strnz;
--- src/type_base_compare.h
+++ src/type_base_compare.h
@@ -37,6 +37,231 @@
 
 #ifndef TYPE_BASE_COMPARE_H
 #define TYPE_BASE_COMPARE_H
+/* stddef.h:
+ *   - NULL
+ *   - ptrdiff_t
+ *   - size_t
+ */
+#include <stddef.h>
+
 #include "base.h"
+
+#include "ptrs.h"
+#include "bits.h"
+
+/* ---------------------------------------------------------------- */
+/* Dependencies.                                                    */
+/* ---------------------------------------------------------------- */
+
+#include "type_base_prim.h"
+#include "type_base_typed.h"
+#include "type_base_tval.h"
+
+/* ---------------------------------------------------------------- */
+/* Orderings: comparison descriptions.                              */
+/* ---------------------------------------------------------------- */
+
+typedef signed int ordering_t;
+
+#if 0
+#define ORDERING_ERR_1    ((int) -0x37ED)
+#define ORDERING_ERR_2    ((int) -0x37EE)
+#define ORDERING_LT_LOSSY ((int) -0x37EF)
+#endif
+
+#define ORDERING_ERR_1    ((int) -(((int) 0x37ED) << (8 * (SIZE_LESS_NULL(SIZE_LESS_NULL(sizeof(int)))))))
+#define ORDERING_ERR_2    ((int) -(((int) 0x37EE) << (8 * (SIZE_LESS_NULL(SIZE_LESS_NULL(sizeof(int)))))))
+#define ORDERING_LT_LOSSY ((int) -(((int) 0x37EF) << (8 * (SIZE_LESS_NULL(SIZE_LESS_NULL(sizeof(int)))))))
+int ordering_err_1   (void);
+int ordering_err_2   (void);
+int ordering_lt_lossy(void);
+
+#define IS_ORDERING_SUCCESS(ordering) (((ordering) != (ORDERING_ERR_1   )) && ((ordering) != (ORDERING_ERR2)))
+#define IS_ORDERING_ERROR(  ordering) (((ordering) == (ORDERING_ERR_1   )) || ((ordering) == (ORDERING_ERR2)))
+#define IS_ORDERING_LOSSY(  ordering)  ((ordering) == (ORDERING_LT_LOSSY))
+int is_ordering_success(int ordering);
+int is_ordering_error  (int ordering);
+int is_ordering_lossy  (int ordering);
+
+/* If the result corresponds to an error code, return ORDERING_LT_LOSSY. */
+#define ORDERING_SUCCESS(ordering)   \
+  ( (IS_ORDERING_SUCCESS((ordering)) \
+  ? (ordering)                       \
+  : (ORDERING_LT_LOSSSY)             \
+  )
+int ordering_success(int ordering);
+
+#define ORDERING_LT (-1)
+#define ORDERING_EQ ( 0)
+#define ORDERING_GT ( 1)
+int ordering_lt(void);
+int ordering_eq(void);
+int ordering_gt(void);
+
+
+enum ordering_relation_e
+{
+  ordering_lt    = ORDERING_LT,
+  ordering_eq    = ORDERING_EQ,
+  ordering_gt    = ORDERING_GT,
+
+  ordering_err_1 = ORDERING_ERR_1,
+  ordering_err_2 = ORDERING_ERR_2,
+
+  ordering_stop
+};
+typedef enum ordering_relation_e ordering_relation_t;
+
+#define ORDERING_RELATION(ordering) \
+  ( (IS_ORDERING_ERROR((ordering))) \
+  ? (ordering)                      \
+  : (SIGN((ordering))               \
+  )
+ordering_relation_t ordering_relation(int ordering);
+
+/* Obtain a successful ordering value based on direct application of the "<="
+ * and "<" binary operators.
+ */
+#define ORDERING_CMP(check, baseline) (ORDERING_SUCCESS((CMP(check, baseline))))
+
+/* ---------------------------------------------------------------- */
+/* Comparers.                                                       */
+/* ---------------------------------------------------------------- */
+
+typedef int (*comparer_t)(void *context, const void *check, const void *baseline);
+
+/* TODO: once type_fun is written, make this a subtype, in the .c file, of
+ * "callback", which is pretty much the same but has just "funp_t" instead!
+ */
+typedef struct cmp_callback_s cmp_callback_t;
+struct cmp_callback_s
+{
+  comparer_t  comparer;
+  void       *comparer_context;
+};
+
+/* ---------------------------------------------------------------- */
+/* Various comparers.                                               */
+/* ---------------------------------------------------------------- */
+
+/* with_type:     Elements are in the array.       */
+/* ref_with_type: Elements are pointers to values. */
+int compare_with_type    (void *context, const     void         *check, const          void         *baseline);
+int compare_ref_with_type(void *context, const     void        **check, const          void        **baseline);
+
+int compare_tval    (void *context, const          tval         *check, const          tval         *baseline);
+int compare_tvalr   (void *context, const          tval        **check, const          tval        **baseline);
+
+int compare_char    (void *context, const          char         *check, const          char         *baseline);
+int compare_schar   (void *context, const signed   char         *check, const signed   char         *baseline);
+int compare_uchar   (void *context, const unsigned char         *check, const unsigned char         *baseline);
+int compare_short   (void *context, const          short        *check, const          short        *baseline);
+int compare_ushort  (void *context, const unsigned short        *check, const unsigned short        *baseline);
+int compare_int     (void *context, const          int          *check, const          int          *baseline);
+int compare_uint    (void *context, const unsigned int          *check, const unsigned int          *baseline);
+int compare_long    (void *context, const          long         *check, const          long         *baseline);
+int compare_ulong   (void *context, const unsigned long         *check, const unsigned long         *baseline);
+int compare_float   (void *context, const          float        *check, const          float        *baseline);
+int compare_double  (void *context, const          double       *check, const          double       *baseline);
+int compare_ldouble (void *context, const long     double       *check, const long     double       *baseline);
+
+int compare_size    (void *context, const          size_t       *check, const          size_t       *baseline);
+int compare_ptrdiff (void *context, const          ptrdiff_t    *check, const          ptrdiff_t    *baseline);
+
+int compare_objp    (void *context, const          void        **check, const          void        **baseline);
+int compare_funp    (void *context, const          funp_cast_t  *check, const          funp_cast_t  *baseline);
+
+/* mem:  Directly compare the elements' bytes in memory.                       */
+/* memr: Elements are "void *" pointers; compare the referent areas of memory. */
+int compare_mem     (void *context, const          void         *check, const          void         *baseline);
+int compare_memr    (void *context, const          void        **check, const          void        **baseline);
+
+/* The strings are contained in the elements themselves. */
+int compare_strn    (void *context, const          char         *check, const          char         *baseline);
+int compare_strz    (void *context, const          char         *check, const          char         *baseline);
+
+/* Elements are "const char *". */
+int compare_strnr   (void *context, const          char        **check, const          char        **baseline);
+int compare_strzr   (void *context, const          char        **check, const          char        **baseline);
+
+/* ---------------------------------------------------------------- */
+
+void *compare_with_type_context    (const type_t *type);
+void *compare_ref_with_type_context(const type_t *type);
+
+void *compare_tval_context   (void);
+void *compare_tvalr_context  (void);
+
+void *compare_char_context   (void);
+void *compare_schar_context  (void);
+void *compare_uchar_context  (void);
+void *compare_short_context  (void);
+void *compare_ushort_context (void);
+void *compare_int_context    (void);
+void *compare_uint_context   (void);
+void *compare_long_context   (void);
+void *compare_ulong_context  (void);
+void *compare_float_context  (void);
+void *compare_double_context (void);
+void *compare_ldouble_context(void);
+
+void *compare_size_context   (void);
+void *compare_ptrdiff_context(void);
+
+void *compare_objp_context   (void);
+void *compare_funp_context   (void);
+
+void *compare_mem_context    (size_t n);
+void *compare_memr_context   (size_t n);
+
+void *compare_strn_context   (size_t n);
+void *compare_strz_context   (void);
+
+void *compare_strnr_context  (size_t n);
+void *compare_strzr_context  (void);
+
+/* ---------------------------------------------------------------- */
+
+extern const comparer_t comparer_with_type;
+extern const comparer_t comparer_ref_with_type;
+
+extern const comparer_t comparer_tval;
+extern const comparer_t comparer_tvalr;
+
+extern const comparer_t comparer_char;
+extern const comparer_t comparer_schar;
+extern const comparer_t comparer_uchar;
+extern const comparer_t comparer_short;
+extern const comparer_t comparer_ushort;
+extern const comparer_t comparer_int;
+extern const comparer_t comparer_uint;
+extern const comparer_t comparer_long;
+extern const comparer_t comparer_ulong;
+extern const comparer_t comparer_float;
+extern const comparer_t comparer_double;
+extern const comparer_t comparer_ldouble;
+
+extern const comparer_t comparer_size;
+extern const comparer_t comparer_ptrdiff;
+
+extern const comparer_t comparer_objp;
+extern const comparer_t comparer_funp;
+
+extern const comparer_t comparer_mem;
+extern const comparer_t comparer_memr;
+
+extern const comparer_t comparer_strn;
+extern const comparer_t comparer_strz;
+
+extern const comparer_t comparer_strnr;
+extern const comparer_t comparer_strzr;
+
+/* ---------------------------------------------------------------- */
+/* Post-dependencies.                                               */
+/* ---------------------------------------------------------------- */
+
+#ifdef TODO
+#include "type_base_type.h"
+#endif /* #ifdef TODO */
 
 #endif /* ifndef TYPE_BASE_COMPARE_H */
